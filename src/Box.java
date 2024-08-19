fa7914c4b89e7426e1cbc151bb698b404d2b3df0
Ruslan <SolovyovRI@yandex.ru>
2024-08-19 09:53:55 +0300

    ссылочные типы в методах и конструкторах + hw

--- src/Box.java
+++ src/Box.java
@@ -17,11 +17,54 @@ public class Box {
     Box(double size) {
         this(size, size, size);
     }
+    // конструктор с ссылочным типом
+    Box(Box another) {
+        this(another.length, another.width, another.height);
+    }
 
     void setDimens(double length, double width, double height) {
         this.length = length;
         this.width = width;
         this.height = height;
+    }
+    // метод может принимать другой ссылочный тип и работать с ним, в данном случае это Box another
+    void compare(Box another){
+        double currentVolume = getVolume();
+        double anotherVolume = another.getVolume();
+        if (currentVolume > anotherVolume) {
+            System.out.println("Current > Another");
+        }
+        else if (currentVolume < anotherVolume) {
+            System.out.println("Current < Another");
+        }
+        else if (currentVolume == anotherVolume) {
+            System.out.println("Current = Another");
+        }
+    }
+
+    int compareTwo(Box another) {
+        double currentVolume = getVolume();
+        double anotherVolume = another.getVolume();
+        int result;
+        if (currentVolume > anotherVolume) {
+            result = 1;
+        }
+        else if (currentVolume < anotherVolume) {
+            result = -1;
+        }
+        else {
+            result = 0;
+        }
+        return result;
+    }
+    // метод возвращения новой коробки Box
+    // создаем новую коробку и добавляем поля из уже созданной коробки lenght, width, height
+    Box copy() {
+        return new Box(this.length, this.width, this.height);
+    }
+    // метод возвращающий обьект Box (current), умноженный на 2
+    Box increase() {
+        return new Box(this.length * 2, this.width * 2, this.height * 2);
     }
 
     double getVolume() {
---
+++ src/Compare.java
@@ -0,0 +1,14 @@
+public class Compare {
+    public static void main(String[] args) {
+        Box current = new Box(10);
+        Box another = current.increase();
+        current.compare(another);
+        // метод умножающий коробку на 2
+        Box increased = current.increase();
+        current.showVolume();
+        another.showVolume();
+        increased.showVolume();
+        int result = current.compareTwo(another);
+        System.out.println(result);
+    }
+}
